0a5b1570f5ad948c23ae7ffdc11be2d3463e9557
Amani Khalid <amanikhalidd@gmail.com>
2025-07-17 09:20:27 +0400

    Modified main structure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿namespace SimpleHotelRoomManagement_OOP
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Security.Principal;
+using System.Transactions;
+
+namespace SimpleHotelRoomManagement_OOP
 {
     internal class Program
     {
