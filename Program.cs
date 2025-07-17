c26764998c9236884af6969c27811c359d46b09d
Amani Khalid <amanikhalidd@gmail.com>
2025-07-17 14:26:18 +0400

    Modified Static Main

--- Program.cs
+++ Program.cs
@@ -3,49 +3,14 @@ using System.Collections.Generic;
 
 namespace SimpleHotelRoomManagement_OOP
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            HotelSystem hotelSystem = new HotelSystem();
-
-            int choice; // Variable to store user choice for menu options
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Welcome to the Hotel Room Management System");
-                Console.WriteLine("1. Add New Room");
-                Console.WriteLine("2. View All Rooms");
-                Console.WriteLine("3. Reserve a Room");
-                Console.WriteLine("4. View All Reservations");
-                Console.WriteLine("5. Search Reservation by Guest Name");
-                Console.WriteLine("6. Find Highest Paying Guest");
-                Console.WriteLine("7. Cancel Reservation");
-                Console.WriteLine("0. Exit");
-
-                Console.Write("Please enter your choice: ");
-
-                int.TryParse(Console.ReadLine(), out choice);
-
-                switch (choice) // Switch case to handle user choicesl
-                {
-                    case 1: hotelSystem.AddRoom(); break;
-                    case 2: hotelSystem.ViewAllRooms(); break;
-                    case 3: hotelSystem.ReserveRoom(); break;
-                    case 4: hotelSystem.ViewAllReservations(); break;
-                    case 5: hotelSystem.SearchReservation(); break;
-                    case 6: hotelSystem.HighestPayingGuest(); break;
-                    case 7: hotelSystem.CancelReservation(); break;
-
-                    case 0: Console.WriteLine("Exiting..."); break;
-                    default: Console.WriteLine("Invalid choice."); break; // Default case for invalid input
-                }
-
-
-            } while
-            (choice != 0);
-        }
+            HotelSystem system = new HotelSystem();
+            system.Run();
+        } 
+    }
 
 
 
@@ -283,4 +248,4 @@ namespace SimpleHotelRoomManagement_OOP
             }
         }
     }
-}
+
