185f9d114b100605a921dcdc3122d0279b9acc97
Amani Khalid <amanikhalidd@gmail.com>
2025-07-17 09:24:37 +0400

    Inserted main menu for the system

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace SimpleHotelRoomManagement_OOP
     {
         static void Main(string[] args)
         {
+            HotelSystem hotelSystem = new HotelSystem();
+            int choice;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Hotel Room Management System");
+                Console.WriteLine("1. Add New Room");
+                Console.WriteLine("2. View All Rooms");
+                Console.WriteLine("3. Reserve a Room");
+                Console.WriteLine("4. View All Reservations");
+                Console.WriteLine("5. Search Reservation by Guest Name");
+                Console.WriteLine("6. Find Highest Paying Guest");
+                Console.WriteLine("7. Cancel Reservation");
+                Console.WriteLine("0. Exit");
+
+                Console.Write("Please enter your choice: ");
+
             
         }
     }
